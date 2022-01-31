a2bff7e50eaf57bf18e09b203153d6401160b432
go2hel <61565872+go2hel@users.noreply.github.com>
2022-01-31 12:48:34 +0530

    Box completed

--- Assigment1_781/main.cpp
+++ Assigment1_781/main.cpp
@@ -5,7 +5,7 @@ int main() {
 
 	Ray ray(Vec(0, 1, 0), Vec(1, 0, 0));
 
-	Box bx(Vec(3, 0, 0), 1, 1, 1, Vec(1, 0, 0), Vec(0, 1, 0));
+	Box bx(Vec(3, 0, 0), 3, 3, 3, Vec(1, 0, 0), Vec(0, 1, 0));
 
 	std::cout << bx.findIntersection(ray) << "\n";
 
